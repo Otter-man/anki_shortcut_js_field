a028302b020ea9255d6bef4d076b3e27aa860025
Otter-man <tereschenko.danila@gmail.com>
2021-05-26 21:59:29 +0700

    saving old working version

--- tester_old.js
+++ tester_old.js
@@ -1,33 +1,38 @@
-
 var keystrokes = [];
 var correct_shortcut = document.getElementById("shortcut").textContent;
-$(window).keydown(function (event) {
-  if (keystrokes.length < correct_shortcut.split(' + ').length) {
-    keystrokes.push(event.key);
-    event.preventDefault();
-  }
-  else if (keystrokes.length >= correct_shortcut.split(' + ').length) {
-    keystrokes = [];
-    keystrokes.push(event.key);
-    event.preventDefault();
-  }
-  setTimeout(() => {
-    keystrokes = [];
-  }, 3000)
-
-  $('#pressed_key').html(keystrokes.join(" + "));
-  document.getElementById("pressed_key").style.visibility = "visible";
-  document.getElementById("pressed_key").style.color = "red";
-  event.preventDefault();
-  if (keystrokes_sorted.toString() == correct_shortcut.split(' + ').sort().toString()) {
-    $('#pressed_key').html(correct_shortcut);
-    document.getElementById("pressed_key").style.color = "green";
-    event.preventDefault();
-  }
-  else {
-    document.getElementById("pressed_key").style.visibility = "visible";
+if (window.SwitcherListener == undefined) {
+  window.SwitcherListener = true
+  $(document).keydown(function (event) {
+    if (keystrokes.length < correct_shortcut.split(' + ').length) {
+      keystrokes.push(event.key);
+      event.preventDefault();
+    }
+    else if (keystrokes.length >= correct_shortcut.split(' + ').length) {
+      keystrokes = [];
+      keystrokes.push(event.key);
+      event.preventDefault();
+    }
     $('#pressed_key').html(keystrokes.join(" + "));
+    document.getElementById("pressed_key").style.visibility = "visible";
     document.getElementById("pressed_key").style.color = "red";
-    event.preventDefault();
-  }
-});
+
+    var keystrokes_sorted = keystrokes.slice().sort()
+    if (keystrokes_sorted.toString().toLowerCase() == correct_shortcut.split(' + ').sort().toString().toLowerCase()) {
+      $('#pressed_key').html(correct_shortcut);
+      document.getElementById("pressed_key").style.color = "green";
+      event.preventDefault();
+    }
+    else {
+      document.getElementById("pressed_key").style.visibility = "visible";
+      $('#pressed_key').html(keystrokes.join(" + "));
+      document.getElementById("pressed_key").style.color = "red";
+      event.preventDefault();
+      if (keystrokes.length <= correct_shortcut.split(' + ').length && document.getElementById("pressed_key").style.color == "red") {
+        setTimeout(() => {
+          keystrokes = [];
+          $('#pressed_key').html(keystrokes.join(" + "));
+        }, 1500);
+      }
+    }
+  })
+};
